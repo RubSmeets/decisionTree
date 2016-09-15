9ecd24ef4ddaf932a4d00851df071d5727f6cf09
Crossmos <ruben.smeets@kuleuven.be>
2016-09-15 10:49:09 +0200

    Fixed parameter parser bug on navigation

--- js/compareFramework.js
+++ js/compareFramework.js
@@ -330,7 +330,7 @@
             var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
             for(i = 0; i < hashes.length; i++) {
                 hash = hashes[i].split('=');
-                if(hash[0] === "frameworks") {
+                if(hash[0] === "frameworks" && hash.length > 1) {
                     vars = hash[1].split(';');
                 }
             }
